01fd32ae729a74539c72985d862b7a44046b66a4
Kishore Narang <narangkishore98@gmail.com>
2020-06-01 20:14:13 -0400

    Initial commit

---
+++ src/main/java/CurrencyConverter.java
@@ -0,0 +1,42 @@
+public class CurrencyConverter
+{
+    private String currencies[] = {
+            "CAD",
+            "ERU",
+            "JOD",
+            "INR"
+    };
+
+    private double conversionRate[] = {
+            0.74,
+            1.11,
+            1.41,
+            0.013
+    };
+
+    //Checks if Currency is available in array or not. returns true if available, false otherwise.
+    public int currencyAvailable(String currency)
+    {
+        for(int i=0;i<currencies.length;i++)
+        {
+            if(currencies[i].equalsIgnoreCase(currency))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public double convert(String from, String to, double amount)
+    {
+        double returner = 0.0;
+        if(currencyAvailable(from)!=-1 && currencyAvailable(to)!=-1)
+        {
+            //converting from to USD
+            returner = conversionRate[currencyAvailable(from)] * amount;
+            //returner is in USD now.
+        }
+        return 0.0;
+    }
+
+}
